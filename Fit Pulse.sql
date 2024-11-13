febffd2bda46763cbfbf719949e5c2c7b90e0603
Lucas Diniz Frota <lucasdinizfrota@gmail.com>
2024-11-13 10:55:46 -0300

    Update Fit Pulse.sql

--- Fit Pulse.sql
+++ Fit Pulse.sql
@@ -5,8 +5,8 @@ use fit_pulse;
 
 create table if not exists plano (
 	id_plano varchar(1) not null,
-    nome_plano varchar(10) not null,
-    valor double not null, 
+    	nome_plano varchar(10) not null,
+    	valor double not null, 
 
 	primary key (id_plano)
 );
@@ -14,54 +14,54 @@ create table if not exists plano (
 create table if not exists aluno (	
 	id_aluno varchar(10) not null,
 	nome_aluno varchar(100) not null,
-    cpf_aluno varchar(20) not null,
-    telefone_aluno varchar(20) not null,
-    Email_aluno varchar(100) not null,
-    status_aluno boolean not null,
-    data_nascimento_aluno date not null,
-    data_inscricao date not null,
-    senha_aluno varchar(100) not null,
-    endereco_aluno varchar(100) not null,
-    genero_aluno varchar(10) not null,
-    id_plano varchar(1) not null,
-    
+    	cpf_aluno varchar(20) not null,
+    	telefone_aluno varchar(20) not null,
+	Email_aluno varchar(100) not null,
+	status_aluno boolean not null,
+	data_nascimento_aluno date not null,
+	data_inscricao date not null,
+	senha_aluno varchar(100) not null,
+	endereco_aluno varchar(100) not null,
+	genero_aluno varchar(10) not null,
+	id_plano varchar(1) not null,
+	    
 	primary key (id_aluno),
-    constraint fk_aluno_plano foreign key (id_plano) references plano(id_plano)
+    	constraint fk_aluno_plano foreign key (id_plano) references plano(id_plano)
     
 );
 
 
 create table if not exists exercicio (
 	id_exercicio varchar(10) not null,
-    nome_exercicio varchar(100) not null,
-    tipo_exercicio varchar(50) not null,
-    maquina_exercicio varchar(100) not null,
-    descricao_exercicio varchar(1000) not null,
+    	nome_exercicio varchar(100) not null,
+    	tipo_exercicio varchar(50) not null,
+    	maquina_exercicio varchar(100) not null,
+    	descricao_exercicio varchar(1000) not null,
+
     
-    
-    primary key (id_exercicio)
+    	primary key (id_exercicio)
 );
 
 create table if not exists pagamento  (
 	id_pagamento varchar(1) not null,
-    tipo_pagamento varchar(10) not null,
-    valor_total double not null,
-    mes_pagamento date not null,	
+    	tipo_pagamento varchar(10) not null,
+    	valor_total double not null,
+    	mes_pagamento date not null,	
     
-    primary key (id_pagamento)
+    	primary key (id_pagamento)
 );
 
 create table if not exists treino (
-	id_treino varchar(10) not null,
+	id_aluno varchar(10) not null,
 	id_exercicio varchar(10) not null,
-    id_aluno varchar(10) not null,
-    serie_exercicio int not null,
-    repeticoes_exercicio int not null,
-    tempo_exercicio int not null,
+    	serie_exercicio int not null,
+    	repeticoes_exercicio int not null,
+    	tipo_treino varchar(1) not null,
     
-    primary key (id_treino, id_exercicio, id_aluno),
-    constraint fk_exercicio_aluno foreign key (id_aluno) references aluno(id_aluno),
-    constraint fk_aluno_exercicio foreign key (id_exercicio) references exercicio(id_exercicio)
+    
+    	primary key (id_aluno, id_exercicio),
+    	constraint fk_treino_aluno foreign key (id_aluno) references aluno(id_aluno),
+    	constraint fk_treino_exercicio foreign key (id_exercicio) references exercicio(id_exercicio)
     
 );
 
